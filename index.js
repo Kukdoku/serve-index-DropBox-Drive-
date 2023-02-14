47f2b5c2b4a13f345a6526327aba3ebd97f733dc
fun zone club <visheshmzn99@gmail.com>
2023-02-14 10:13:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const app = express();
+
+app.use('/ftp',express.static('public'), serveIndex('public', {icons:true}))
+
+app.listen(3000, ()=>console.log('connected'))
